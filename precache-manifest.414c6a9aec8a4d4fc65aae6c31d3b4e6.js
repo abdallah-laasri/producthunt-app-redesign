cd207d0b7a3e01ff07673956e5e5bccf43d675b8
abdallahlaasri <abdallahlaasri@gmail.com>
2020-02-04 18:32:18 +0100

    Updates

---
+++ precache-manifest.414c6a9aec8a4d4fc65aae6c31d3b4e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eefefd2aa334ad3b095b6bcf2ae633d",
+    "url": "/producthunt-app-redesign/index.html"
+  },
+  {
+    "revision": "88b6b5a1b5a25235269f",
+    "url": "/producthunt-app-redesign/static/css/main.2d1cf961.chunk.css"
+  },
+  {
+    "revision": "1bd29ea418d5d9194670",
+    "url": "/producthunt-app-redesign/static/js/2.b9d62f91.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/producthunt-app-redesign/static/js/2.b9d62f91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88b6b5a1b5a25235269f",
+    "url": "/producthunt-app-redesign/static/js/main.ed16a4ca.chunk.js"
+  },
+  {
+    "revision": "3e436e802c1f5e9455cd",
+    "url": "/producthunt-app-redesign/static/js/runtime-main.ca4d14b5.js"
+  },
+  {
+    "revision": "183b21250924d6388328e56b89bc0a13",
+    "url": "/producthunt-app-redesign/static/media/proHunt.183b2125.jpeg"
+  }
+]);
